486f63fb67affa930f0362b9b832e65b0606f204
Taewankim <keemtaewan@gmail.com>
2024-10-28 16:12:52 -0700

    add cluster version

--- haversine/src/haversine.cpp
+++ haversine/src/haversine.cpp
@@ -17,23 +17,24 @@ typedef int32_t s32;
 typedef int64_t s64;
 typedef double f64;
 
-const int NUM_POINTS = 10000;
 const int NUM_COORDINATES = 4;
+int NUM_POINTS = 10;
+int TYPE_CLUSTER = 0;
 
-int generate_data(f64* result){
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_real_distribution<> lon_dist(-180.0, 180.0);
-    std::uniform_real_distribution<> lat_dist(-90.0, 90.0);
+int main(int argc, char **argv){
+    if (argc != 4){
+        fprintf(stderr, "Usage: haversine [uniform/cluster] [randomseed] [num of points]\n");
+        return EXIT_FAILURE;
+    }
+    if (strcmp(argv[1], "uniform") == 0) {
+        TYPE_CLUSTER = 0;
+    }
+    else if(strcmp(argv[1], "cluster") == 0) {
+        TYPE_CLUSTER = 1;
+    }
+    int random_seed = std::atoi(argv[2]);
+    NUM_POINTS = std::atoi(argv[3]);
 
-    result[0] = lon_dist(gen);
-    result[1] = lat_dist(gen);
-    result[2] = lon_dist(gen);
-    result[3] = lat_dist(gen);
-    return 1;
-}
-
-int main() {
     FILE *file = fopen("harversine.json", "w");
     if (file == NULL) {
         printf("Unable to create file\n");
@@ -44,11 +45,38 @@ int main() {
     f64 data_ptr[NUM_COORDINATES] = {0.0,};
     f64 sum = 0.0;
     f64 average = 0.0;
+    f64 x_range1,y_range1,x_range2,y_range2;
 
+    // std::random_device rd;
+    std::mt19937 gen(random_seed);
+    if (TYPE_CLUSTER == 1){
+        int NUM_CLUSTER = 4;
+        std::uniform_int_distribution<> dis(0, NUM_CLUSTER - 1);
+        int x_random = dis(gen);
+        int y_random = dis(gen);
+        printf("x_random: %d, y_random: %d\n",x_random,y_random);
+        x_range1 = -180.0 + 360 / NUM_CLUSTER * x_random;
+        x_range2 = -180.0 + 360 / NUM_CLUSTER * (x_random+1);
+        y_range1 = -90.0 + 180 / NUM_CLUSTER * y_random;
+        y_range2 = -90.0 + 180 / NUM_CLUSTER * (y_random+1);
+    }
+    else{
+        x_range1 = -180.0;
+        x_range2 = 180.0;
+        y_range1 = -90.0;
+        y_range2 = 90.0;
+    }
+    std::uniform_real_distribution<> lon_dist(x_range1, x_range2);
+    std::uniform_real_distribution<> lat_dist(y_range1, y_range2);
 
     fprintf(file, "{\"pairs\":[\n");
     for (int i=0;i<NUM_POINTS;++i){
-        generate_data(data_ptr);
+        // generate_data(data_ptr, gen);
+        data_ptr[0] = lon_dist(gen);
+        data_ptr[1] = lat_dist(gen);
+        data_ptr[2] = lon_dist(gen);
+        data_ptr[3] = lat_dist(gen);
+
         fprintf(file, "    {\"x0\":%lf, \"y0\":%lf, \"x1\":%lf, \"y1\":%lf}",data_ptr[0],data_ptr[1],data_ptr[2],data_ptr[3]);
         if (i < NUM_POINTS - 1){
             fprintf(file, ",");
@@ -65,5 +93,5 @@ int main() {
     printf("Expected sum: %lf\n",average);
 
     return 0;
+    return EXIT_SUCCESS;
 }
-
